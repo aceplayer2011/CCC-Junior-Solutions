173144cf94b19f6c2e3094f364d178d54e483b68
ynm_011 <82189006+aceplayer2011@users.noreply.github.com>
2024-12-28 21:34:54 +0000

    finish J4

--- 2003 Junior/J1.py
+++
@@ -1,9 +0,0 @@
-bar = int(input())
-space = int(input())
-handle = int(input())
-
-for i in range(bar):
-    print("*" + " "*space + "*" + " "*space + "*")
-print("*"*(space*2+3))
-for i in range(handle):
-    print(" "*(space+1) + "*")
--- 2009 Junior/J1.py
+++
@@ -1,4 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-print('The 1-3-sum is', 91 + (a*1) + (b*3) + (c*1))
