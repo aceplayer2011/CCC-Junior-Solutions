cf28a3c84492a41604df7242c352996625585795
ynm_011 <82189006+aceplayer2011@users.noreply.github.com>
2024-12-29 05:40:11 +0000

    J4

---
+++ 2001 Junior/J3-S1.py
@@ -0,0 +1,27 @@
+cards = input()
+idx_d = cards.find('D')
+idx_h = cards.find('H')
+idx_s = cards.find('S')
+cs = list(cards[1:idx_d])
+ds = list(cards[idx_d+1: idx_h])
+hs = list(cards[idx_h+1: idx_s])
+ss = list(cards[idx_s+1:])
+
+
+def count_pts(suit):
+    pts = 0
+    if 'A' in suit:
+        pts += 4
+    if 'K' in suit:
+        pts += 3
+    if 'Q' in suit:
+        pts += 2
+    if 'J' in suit:
+        pts += 1
+    if len(suit) == 0:
+        pts += 3
+    elif len(suit) == 1:
+        pts += 2
+    elif len(suit) == 2:
+        pts += 1
+    return pts
